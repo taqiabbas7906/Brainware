fa5e7f4628157f0f31b521703ca479558a18fcde
Syed Taqi Abbas <taqiabbas8989@gmail.com>
2024-03-06 23:33:58 +0500

    Completed benefits section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./Components/Button";
 import Header from "./Components/Header";
+import Benefits from "./Components/Benefits"
 import Hero from "./Components/Hero";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefits/>
       </div>
       <ButtonGradient />
     </>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          } fixed top-[4.5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
           <ul className="reletive z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
